9f7d7fa5c53a69ed128b505b62d90f2af96f40f8
finanovita <finanovita479@gmail.com>
2024-05-11 12:32:59 +0700

    Menambahkan default pada fungsi main

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -106,12 +106,14 @@ int main() {
 		case 4:
 			cout << "Exiting program." << endl;
 			break;
-		
+		default:
+			cout << "Invalid choice. Try again." << endl;
+			break;
 		}
 
-		
+		cout << endl;
 	}
 
-	
+	return 0;
 }
 
