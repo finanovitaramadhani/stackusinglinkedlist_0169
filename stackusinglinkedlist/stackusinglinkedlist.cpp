500238607541f16097ed383d986f1f163a7a91c8
finanovita <finanovita479@gmail.com>
2024-05-11 12:27:27 +0700

    conditional statement if pada case 3

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -94,6 +94,10 @@ int main() {
 				cout << "Stack is empty. Cannot pop." << endl;
 			}
 			break;	
+		case 3:
+			if (!stack.isEmpty()) {
+				stack.peek(); // Get the value of the top element
+			}
 
 
 		}
